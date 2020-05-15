5429d824adf99c6ecb1350e7c6943c8a0072140b
Redon Osmanollaj <redon_osmanollaj@hotmail.com>
2020-05-15 02:11:51 +0200

    Structure folders

---
+++ app/src/main/java/com/example/barbershop/auth/http/LoginHttpClient.java
@@ -0,0 +1,4 @@
+package com.example.barbershop.auth.http;
+
+public class LoginHttpClient {
+}
---
+++ app/src/main/java/com/example/barbershop/auth/ui/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.example.barbershop.auth.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.barbershop.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
